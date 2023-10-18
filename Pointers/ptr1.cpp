850ff950745e0ea47d6a7fa44e861ee643a4c784
sonakshi01 <sonakshi24oct04@gmail.com>
2023-10-18 16:25:31 +0530

    cha+nges

--- Pointers/ptr1.cpp
+++ Pointers/ptr1.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
 using namespace std;
 
+void print(int arr[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i];
+    }
+
+}
+
 int main(){
+    int arr[5]={2,5,6,4,8};
+    print(arr,5);
     
 }
